55de9c1185877e476bd3df0cf83c67682f1eb8d2
Miles Bradt <smbradtmichael@gmail.com>
2018-11-02 08:43:47 -0700

    add basic constuctors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function Customer(name, number, total) {
+  this.name = name,
+  this.number = number,
+  this.total = total
+}
+
+function Pizza(toppings, size) {
+  this.toppings = [],
+  this.size = size
+}
+
+
 $(document).ready(function() {
 
 });

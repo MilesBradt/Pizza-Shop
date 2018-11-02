66c2034c407c4cf4326159eccda30c66b70d3e34
Miles Bradt <smbradtmichael@gmail.com>
2018-11-02 09:43:28 -0700

    split toppings for multiple inputs

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,35 @@ function Customer(name, number, total) {
   this.total = total
 }
 
-function Pizza(toppings, size, price = 7) {
+function Pizza(toppings, size, price = 0) {
   this.toppings = [],
   this.size = size,
   this.price = price
 }
 
-Pizza.prototype.addToppings = function(topping){
-  this.toppings.push(topping)
+Pizza.prototype.addToppings = function(topping) {
+  var splitToppings = topping.split(" ");
+  this.toppings.push(splitToppings)
+}
+
+Pizza.prototype.addSize = function(size) {
+  this.size = size
 }
 
 Pizza.prototype.determinePrice = function() {
   var newPrice = this.price
+  if (this.size === "medium") {
+    newPrice += 7
+  } else if (this.size === "large") {
+    newPrice += 10
+  }
+
   for (var i = 0; i < this.toppings.length; i++) {
     if (this.toppings[i] === "pepperoni") {
-      newPrice += 1;
+      newPrice += 1
     }
   }
-  return newPrice;
+  return newPrice
 }
 
 var newCustomer = new Customer();
